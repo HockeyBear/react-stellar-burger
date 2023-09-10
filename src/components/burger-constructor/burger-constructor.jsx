68a47e9720d1bbd8d4aa0bc4faf6b4678aa0f1e5
HockeyBear <nikita.efremov.02@mail.ru>
2023-09-10 23:35:31 +0500

    constructor no ready

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,15 +1,73 @@
-import React from "react";
+import React, { Component, useMemo } from "react";
 import constructorStyles from './burger-constructor.module.css';
-import { Counter, Tab, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import { constructorPropType } from '../../utils/prop-types';
+import { ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import ingredientPropType from '../../utils/prop-types';
 import PropTypes from 'prop-types';
 
-function BurgerConstructor(props) {
+const BurgerConstructor = (props) => {
+  
+  const totalPrice = props.data.reduce((sum, item) => sum + item.price, 0);
+
+  const bun = useMemo(() => {
+    return props.data.find((item) => item.type === 'bun');
+  }, [props]);
+  const sauce = React.useMemo(() => {
+    return props.data.find(item => item.type === 'sauce')
+  }, [props]);
+  const main = React.useMemo(() => {
+    return props.data.filter(item => item.type === 'filling')
+  }, [props]);
+
+  const RenderedIngredient = (item, type) => {
+    return (
+      <div key={item._id}>
+        <DragIcon type="primary" />
+        <ConstructorElement text={type.name} price={type.price} thumbnail={type.image} />
+      </div>
+    )
+  }
+
   return (
     <section className={constructorStyles.section}>
-
+      <div className={constructorStyles.box + ' custom-scroll'}>
+        <div className={constructorStyles.ingredient}>
+          {bun && <ConstructorElement type="top" isLocked={true}
+            text={bun.name + "(верх)"}
+            price={bun.price}
+            thumbnail={bun.image}
+          />}
+        </div>
+        <div className={constructorStyles.ingredient}>
+          <DragIcon type="primary" />
+          {bun && <ConstructorElement text={sauce.name}
+            price={sauce.price}
+            thumbnail={sauce.image}
+          />}
+        </div>
+        {main.map(item => <RenderedIngredient key={item._id} {...item} />)}
+        <div className={constructorStyles.ingredient}>
+          {bun && <ConstructorElement type="bottom" isLocked={true}
+            text={bun.name + "(низ)"}
+            price={bun.price}
+            thumbnail={bun.image}
+          />}
+        </div>
+      </div>
+      <div className={constructorStyles.ordering}>
+        <div className={constructorStyles.sum}>
+          <p className="text text_type_main-large">{totalPrice}</p>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button htmlType="button" type="primary" size="large" onClick={() => props.setIsOpen(true)}>
+          Оформить заказ
+        </Button>
+      </div>
     </section>
   )
+}
+
+BurgerConstructor.propTypes = {
+  data: PropTypes.arrayOf(ingredientPropType).isRequired
 }
 
 export default BurgerConstructor;
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,12 +1,12 @@
-import {React,  useState } from "react";
+import React, { useState } from "react";
 import ingStyles from './burger-ingredients.module.css';
 import { Counter, Tab, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import { ingredientPropType } from '../../utils/prop-types';
+import ingredientPropType from '../../utils/prop-types';
 import PropTypes from 'prop-types';
 
 function Ingredient (data) {
   return(
-    <div className={ingStyles.ingredient_card}>
+    <div className={ingStyles.ingredient_card} key={data._id}>
       <Counter count={1} size="default" extraClass="m-1" />
       <img src={data.image} alt={data.name} />
       <div>
@@ -34,23 +34,23 @@ function BurgerIngredients(props) {
     <section className={ingStyles.section}>
       <h2 className="text text_type_main-large pt-10">Соберите бургер</h2>
       <div className={`${ingStyles.tab} pt-5 pb-10`}>
-        <Tab value="bun" active={current === 'bun'} onClick={handleClickTab}>Булки</Tab>
-        <Tab value="sauce" active={current === 'sauce'} onClick={handleClickTab}>Соусы</Tab>
-        <Tab value="filling" active={current === 'filling'} onClick={handleClickTab}>Начинки</Tab>
+        <Tab value="bun" active={current === "bun"} onClick={handleClickTab}>Булки</Tab>
+        <Tab value="sauce" active={current === "sauce"} onClick={handleClickTab}>Соусы</Tab>
+        <Tab value="filling" active={current === "filling"} onClick={handleClickTab}>Начинки</Tab>
       </div>
       <div className={ingStyles.ingredients}>
         <div className={`${ingStyles.ingredient} custom-scroll`}>
           <h2 className="text text_type_main-medium">Булки</h2>
           <div className={`${ingStyles.ingredient_con} pt-6 pl-4 pb-10`}>
-            {bun.map(item => <Ingredient {...item} />)}
+            {bun.map(item => <Ingredient key={item._id} {...item} />)}
           </div>
           <h2 className="text text_type_main-medium">Соусы</h2>
           <div className={`${ingStyles.ingredient_con} pt-6 pl-4 pb-10`}>
-            {sauce.map(item => <Ingredient {...item} />)}
+            {sauce.map(item => <Ingredient key={item._id} {...item} />)}
           </div>
           <h2 className="text text_type_main-medium">Начинки</h2>
           <div className={`${ingStyles.ingredient_con} pt-6 pl-4 pb-10`}>
-            {filling.map(item => <Ingredient {...item} />)} 
+            {filling.map(item => <Ingredient key={item._id} {...item} />)} 
           </div>
         </div>
       </div>
@@ -62,7 +62,7 @@ Ingredient.propTypes = {
   item: ingredientPropType
 };
 BurgerIngredients.propTypes = {
-  data: PropTypes.arrayOf(ingredientPropType)
+  data: PropTypes.arrayOf(ingredientPropType).isRequired
 };
 
 export default BurgerIngredients;
